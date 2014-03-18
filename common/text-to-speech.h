4d4f4ab2979f7ea45d5b40f71cb171fb933e3287
Thierry Crozat <criezy@scummvm.org>
2014-03-18 19:04:37 +0000

    TTS: Add some random thoughts on the TTS Abstract Interface
    This should make it clear that this is a prototype and there are many
    questions that needs to be looked at.

--- common/text-to-speech.h
+++ common/text-to-speech.h
@@ -37,13 +37,30 @@ namespace Common {
  */
 class TextToSpeechManager {
 public:
+	// This is a simple interface for TTS capabilities, probably too simple.
+	// Here are some questions we should look at:
+	// - Encoding of the string passed to initSpeech()
+	// - The TTS engine can have additional settings, such as the tempo (e.g. "Speaking Rate" on OS X TTS)
+	// - Do we need the possibility to list only male or female voices?
+	// - Should we make it easy for several sub-systems to use the TTS engine, for example an engine to use TTS
+	//   in the game language and when the GMM pops up it can speak the button text using the GUI language and
+	//   when close it switch backs to the game language.
+	//   If so I can see several ways to handle this. For example:
+	//   - use a stack of voice settings. When a setting is pushed it calls initSpecch() with it, when it is popped
+	//     it calls again initSpeech() with the previous one (if any).
+	//   - use a context. We set the voice settings for a context and then the context should be passed to startSpeech().
+	//     If it is different from the previous context it would call initSpeech().
+	//   Either way this could be handled by the base class with derive classes only needing to implement initSpeech()
+	//   (and maybe clearSpeech()).
+	// - This implementation assumes only one Speech Synthesizer engine is available. Do we want the possibility to have
+	//   more than one and let the user select which ones he want to use (or a priority order and then it uses the one
+	//   with the highest priority that supports the requested language).
+	
 	TextToSpeechManager() {}
 	virtual ~TextToSpeechManager() {}
 		
 	virtual bool initSpeech(Language, const String &voice = String()) {return false;}
 	virtual bool startSpeech(const String&) {return false;}
-		// Note: how should we handle the encoding for the string passed to startSpeech()?
-		// Should we add an argument to the function?
 	virtual void stopSpeech() {}
 	virtual bool isSpeaking() const {return false;}
 
